53fb06bf09dbc8afde2de775fdcb7b674cb6cfd0
JF Padilla <emailreel@gmail.com>
2019-11-11 21:24:31 -0500

    Complete task L3.3 with unique_lock.

--- src/Intersection.cpp
+++ src/Intersection.cpp
@@ -71,9 +71,15 @@ std::vector<std::shared_ptr<Street>> Intersection::queryStreets(std::shared_ptr<
 // adds a new vehicle to the queue and returns once the vehicle is allowed to enter
 void Intersection::addVehicleToQueue(std::shared_ptr<Vehicle> vehicle)
 {
-    // L3.3 : Ensure that the text output locks the console as a shared resource. Use the mutex _mtxCout you have added to the base class TrafficObject in the previous task. Make sure that in between the two calls to std-cout at the beginning and at the end of addVehicleToQueue the lock is not held.
+    // L3.3 : Ensure that the text output locks the console as a shared
+    // resource. Use the mutex _mtxCout you have added to the base class
+    // TrafficObject in the previous task. Make sure that in between the two
+    // calls to std-cout at the beginning and at the end of addVehicleToQueue
+    // the lock is not held.
 
+    std::unique_lock<std::mutex> lck(_mtxCout);
     std::cout << "Intersection #" << _id << "::addVehicleToQueue: thread id = " << std::this_thread::get_id() << std::endl;
+    lck.unlock();
 
     // add new vehicle to the end of the waiting line
     std::promise<void> prmsVehicleAllowedToEnter;
@@ -82,6 +88,8 @@ void Intersection::addVehicleToQueue(std::shared_ptr<Vehicle> vehicle)
 
     // wait until the vehicle is allowed to enter
     ftrVehicleAllowedToEnter.wait();
+    
+    lck.lock();
     std::cout << "Intersection #" << _id << ": Vehicle #" << vehicle->getID() << " is granted entry." << std::endl;
 }
 
